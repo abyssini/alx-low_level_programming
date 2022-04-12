0e61b730bd6b31d2dee2d0446c4d85201b2487cf
abyssini <abyssiniazelalem874@gmail.com>
2022-04-12 10:07:20 -0700

    nasted loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,9 @@
 * Return : 0 
 */
 int main(void)
+
 {
+
   _putchar('_');
   _putchar('p');
   _putchar('u');
@@ -15,5 +17,7 @@ int main(void)
   _putchar('a');
   _putchar('r');
   _putchar('\n');   
+
   return (0);
+
 }
