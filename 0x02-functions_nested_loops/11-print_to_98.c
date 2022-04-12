3dc50876720e66db14a3b6c8e564fa678444d3d7
abyssini <abyssiniazelalem874@gmail.com>
2022-04-12 10:15:40 -0700

    nasted loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,20 +5,31 @@
 * @n: starting number
 */
 void print_to_98(int n)
+
 {
-int i;
-if (n <= 98)
+
+	int i;
+
+	if (n <= 98)
 {
-for (i = n; i < 98; i++)
+
+	for (i = n; i < 98; i++)
 {
-printf("%i, ", i);
+
+	printf("%i, ", i);
 }
 else
 {
-for (i = n; i > 98; i--)
+
+	for (i = n; i > 98; i--)
 {
-printf("%i, ", i);
+
+	printf("%i, ", i);
+
 }
 }
+
 printf("98\n");
+
 }
+
