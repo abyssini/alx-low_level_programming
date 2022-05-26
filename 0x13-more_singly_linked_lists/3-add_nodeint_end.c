2be74975a3c33940cf347321c39f81161c6e8661
abyssini <abyssiniazelalem874@gmail.com>
2022-05-25 17:33:05 -0700

    0x13

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,32 +1,42 @@
 #include "lists.h"
+
 /**
- * add_nodeint_end - adds a node at the end of a linked list
- * @head: pointer to the first element in the list
- * @n: data to insert in the new element
- * Return: pointer to the new node, or NULL if it fails
+ * add_nodeint_end - Function adds new node at the end of a listint_t list
+ * @head: double ptr to start of list
+ * @n: the int t put in new node
+ *
+ * Return: address of new node or NULL
  */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	/* declarations */
 	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *location;
 
+	/* check for null pointer */
+	if (!head)
+		return (NULL);
+	/* malloc space for new node */
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
-
+	/* populate new node with int, NULL */
 	new->n = n;
 	new->next = NULL;
-
-	if (*head == NULL)
+	if (!*head)
 	{
 		*head = new;
 		return (new);
 	}
-
-	while (temp->next)
-		temp = temp->next;
-
-	temp->next = new;
-
+	/* insulate head  & move to end */
+	location = *head;
+	while (location->next)
+	{
+		location = location->next;
+	}
+	/* make last node point to new node */
+	location->next = new;
+	/* return new node*/
 	return (new);
 }
