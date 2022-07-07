f4c27d6067f2de2d42b3494641566ad3125d7844
abyssini <abyssiniazelalem874@gmail.com>
2022-07-07 10:00:38 -0700

    0x17

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,4 +24,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		while (h->next != NULL)
 		{
+			h = h->next;
+		}
+		h->next = new_node;
+		new_node->prev = h;
+	}
+	else
+		*head = new_node;
+
+	return (new_node);
+}
 
